e3f672260a0a97a7810a03b910085da10ccd320b
Wuxialang zhou <wuxialangzhou@Wuxialangs-MacBook-Pro.local>
2020-05-26 19:27:56 +0800

    x

---
+++ etm.py
@@ -0,0 +1,137 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import layers
+from tensorflow.keras import backend as K
+
+
+class SoftmaxWithMask(tf.keras.layers.Layer):
+    def call(self, inputs: tf.Tensor, mask, axis):
+        mask = tf.cast(mask, inputs.dtype)
+        mask = tf.expand_dims(mask, -1)
+        masked_logits = tf.multiply(tf.exp(inputs), mask)
+        softmax = tf.divide(masked_logits, tf.reduce_sum(masked_logits, axis, keepdims=True) + 1e-5)
+        return softmax
+
+
+class Reparameterize(layers.Layer):
+    def call(self, inputs, training=None):
+        mu = inputs[0]
+        logvar = inputs[1]
+        batch = tf.shape(mu)[0]
+        dim = tf.shape(mu)[1]
+        if training is None:
+            training = K.learning_phase()
+        if training:
+            std = tf.exp(0.5 * logvar)
+            eps = tf.keras.backend.random_normal(shape=(batch, dim))
+            return tf.add(tf.multiply(eps, std), mu)
+        else:
+            return mu
+
+
+class Encoder(layers.Layer):
+    def __init__(self, num_topic, t_hidden_size, name, activation, enc_drop, **kwargs):
+        super(Encoder, self).__init__(name=name, **kwargs)
+        self.dense_proj_1 = layers.Dense(t_hidden_size, activation=activation)
+        self.dense_proj_2 = layers.Dense(num_topic, activation=activation)
+        self.dropout_1 = layers.Dropout(enc_drop)
+        self.dropout_2 = layers.Dropout(enc_drop)
+
+        self.dense_mean = layers.Dense(num_topic)
+        self.dense_log_var = layers.Dense(num_topic)
+
+    def call(self, inputs):
+        x = self.dense_proj_1(inputs)
+        x = self.dropout_1(x)
+        x = self.dense_proj_2(x)
+        x = self.dropout_2(x)
+        mu_theta = self.dense_mean(x)
+        logsigma_theta = self.dense_log_var(x)
+        kl_theta = -0.5 * tf.reduce_mean(1 + logsigma_theta - tf.pow(mu_theta, 2) - tf.exp(logsigma_theta),
+                                         axis=-1)
+        return mu_theta, logsigma_theta, kl_theta
+
+
+class Decoder(layers.Layer):
+    def call(self, inputs, **kwargs):
+        theta, beta = inputs
+        res = tf.matmul(theta, beta)
+        return tf.math.log(res + 1e-5)
+
+
+class ETM:
+    def __init__(self, num_topics, vocab_size, t_hidden_size, rho_size,
+                 theta_act, embeddings=None, train_embeddings=True, enc_drop=0.5, seq_length=128):
+        self.num_topics = num_topics
+        self.vocab_size = vocab_size
+        self.t_hidden_size = t_hidden_size
+        self.rho_size = rho_size  # embedding size for both vocabulary & topic
+        self.enc_drop = enc_drop
+        self.seq_length = seq_length
+
+        ## topic_list
+        self.topic_list = tf.constant([list(range(num_topics))])
+
+        ## word embedding
+        if train_embeddings:
+            self.rho = layers.Embedding(self.vocab_size, self.rho_size)
+        else:
+            self.rho = layers.Embedding(self.vocab_size, self.rho_size, weights=[embeddings], trainable=False)
+
+        ## topic embedding matrix
+        self.alpha = tf.Variable(tf.random.normal((num_topics, rho_size)))
+
+        ## vi encoder
+        self.encoder = Encoder(num_topics, t_hidden_size, 'encoder', theta_act, enc_drop)
+
+        ## vi decoder
+        self.decoder = Decoder()
+
+        ## sampling
+        self.sampler = Reparameterize()
+
+    def build(self):
+        input_layer = layers.Input(batch_shape=(None, self.seq_length), dtype=tf.int32)
+        input_mask = layers.Lambda(lambda x: K.cast(K.greater(x, 0), 'int32'), name="input_mask")(input_layer)
+
+        word_emb = self.rho(input_layer)
+        scores = layers.Dense(1, activation='linear')(word_emb)
+        scores = SoftmaxWithMask()(scores, mask=input_mask, axis=1)
+
+        document_embedding = tf.einsum('BTS,BTE->BT', scores, word_emb)
+
+        mu_theta, logsigma_theta, kl_theta = self.encoder(document_embedding)
+
+        z = self.sampler([mu_theta, logsigma_theta])
+        theta = layers.Softmax(axis=-1)(z)  # ( batch, num_topics )
+
+        beta = tf.einsum('TE,VE->TV', self.alpha, self.rho.embeddings)
+        beta = layers.Softmax(axis=-1)(beta)
+
+        lookup_matrix = self.decoder([theta, beta])  # (batch, num_vocab)
+        one_hot_input = tf.one_hot(input_layer, self.vocab_size)
+
+        recon_loss = - tf.einsum('BV,BTV->BT', lookup_matrix, one_hot_input)
+        recon_loss = recon_loss * tf.cast(input_mask, tf.float32)
+        recon_loss = tf.einsum('BT->B', recon_loss) / tf.reduce_sum(tf.cast(input_mask, tf.float32), axis=-1)
+
+        self.model = tf.keras.Model(input_layer, theta)
+        self.model.add_loss(recon_loss + kl_theta)
+
+    def generate_topic_words(self):
+        beta = tf.einsum('TE,VE->TV', self.alpha, self.rho.embeddings)
+        beta = layers.Softmax(axis=-1)(beta)
+        represent_sort = tf.argsort(beta, direction='DESCENDING')
+        represent_sort = represent_sort[:, :20].numpy()
+
+        return represent_sort
+
+
+if __name__ == '__main__':
+    m = ETM(num_topics=30, vocab_size=1000, t_hidden_size=128, rho_size=128, theta_act='relu')
+    m.build()
+    print(m.model.summary())
+    a = np.random.randint(0, 1000, (64, 128))
+    print(m.model.predict(a))
+
+    print(m.generate_topic_words())
---
+++ train.py
@@ -0,0 +1,78 @@
+import argparse
+
+from etm import ETM
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import os
+
+parser = argparse.ArgumentParser(description='The Embedded Topic Model')
+
+### data and file related arguments
+parser.add_argument('--data_path', type=str, default='data/20ng', help='directory containing data')
+parser.add_argument('--emb_path', type=str, default=None,
+                    help='directory containing word embeddings')
+parser.add_argument('--save_path', type=str, default='./results', help='path to save results')
+parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for training')
+parser.add_argument('--vocab_path', type=str, default=None)
+
+### model-related arguments
+parser.add_argument('--num_topics', type=int, default=50, help='number of topics')
+parser.add_argument('--rho_size', type=int, default=300, help='dimension of rho')
+parser.add_argument('--t_hidden_size', type=int, default=800, help='dimension of hidden space of q(theta)')
+parser.add_argument('--theta_act', type=str, default='relu',
+                    help='tanh, softplus, relu, rrelu, leakyrelu, elu, selu, glu)')
+parser.add_argument('--train_embeddings', type=int, default=0, help='whether to fix rho or train it')
+parser.add_argument('--enc_drop', type=float, default=0.0, help='dropout rate on encoder')
+
+### optimization-related arguments
+parser.add_argument('--lr', type=float, default=0.005, help='learning rate')
+parser.add_argument('--epochs', type=int, default=20, help='number of epochs to train...150 for 20ng 100 for others')
+parser.add_argument('--clip', type=float, default=0.0, help='gradient clipping')
+parser.add_argument('--nonmono', type=int, default=10, help='number of bad hits allowed')
+parser.add_argument('--wdecay', type=float, default=1.2e-6, help='some l2 regularization')
+parser.add_argument('--anneal_lr', type=int, default=0, help='whether to anneal the learning rate or not')
+
+args = parser.parse_args()
+
+
+class VisCallback(tf.keras.callbacks.Callback):
+    def __init__(self, etm: ETM, vocab: list, save_path: str):
+        self.etm = etm
+        self.vocab = vocab
+        self.save_path = save_path
+
+    def on_epoch_end(self, epoch, logs=None):
+        if epoch % 10 == 0:
+            topic_rep = self.etm.generate_topic_words()
+            topic_words = [[self.vocab[i] for i in x] for x in topic_rep]
+            for i, topic in enumerate(topic_words):
+                print('topic {}:{}'.format(i, ','.join(topic)))
+            self.etm.model.save_weights(os.path.join(self.save_path, '{}_weight'.format(epoch)))
+
+
+if __name__ == '__main__':
+    if args.emb_path is not None:
+        embedding = np.load(args.emb_path)
+    else:
+        embedding = None
+
+    # build model
+    etm = ETM(num_topics=args.num_topics, rho_size=args.rho_size, theta_act=args.theta_act,
+              train_embeddings=args.train_embeddings, embeddings=embedding, enc_drop=args.enc_drop)
+    etm.build()
+    print(etm.model.summary())
+
+    # loading data
+    data = pd.read_csv(args.data_path, header=None, na_filter=False, delim_whitespace=True, dtype=int).to_numpy()
+    np.random.shuffle(data)
+    vocab = [x.strip() for x in open(args.vocab_path, 'r').readlines()]
+
+    # start training
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+    vis = VisCallback(etm, vocab, args.save_path)
+
+    optimizer = tf.keras.optimizers.Adam(args.lr)
+    etm.model.compile(optimizer=optimizer, loss=tf.keras.losses.MeanSquaredError())
+    etm.model.fit(data, data, epochs=args.epochs, batch_size=args.batch_size, callbacks=[vis])
